31a87d00f2551e1554ed237632ea6bc9b0645b74
mattBr1ght <mateuszwronski.yt@gmail.com>
2024-06-04 22:58:01 +0200

    upload of source code

---
+++ src/lib.rs
@@ -0,0 +1,254 @@
+use std::io::{stdout, stdin, Write};
+
+pub const COLORS :  [&str;8] = [
+"\u{001b}[31m",
+"\u{001b}[34m",
+"\u{001b}[32m",
+"\u{001b}[33m",
+"\u{001b}[35m",
+"\u{001b}[36m",
+"\u{001b}[37m",
+"\u{001b}[30m",
+];
+
+#[derive(PartialEq, Clone, Debug)]
+pub struct Player{
+    pub name: String,
+    color: String,
+}
+
+impl Player{
+    pub fn new(n : String, c : String) -> Player{
+        Player{
+            name: n,
+            color: (c.to_string() + "O\u{001b}[0m"),
+        }
+    }
+
+    pub fn empty() -> Player{
+        Player{
+            name: "{empty}".to_string(),
+            color: "\u{001b}[31mO\u{001b}[0m".to_string(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum GameEnd<'a>{
+    Draw,
+    None,
+    Win(&'a Player),
+}
+
+#[derive(Debug)]
+pub struct GameState<'a>{
+    pub winner: GameEnd<'a>,
+    pub current_player: usize,
+    move_count: u8,
+    board_width: usize,
+    board_height: usize,
+    board: Vec<Vec<Option<&'a Player>>>,
+}
+
+impl<'a> GameState<'a>{
+    
+    pub fn new(w:usize,h:usize) -> GameState<'a>{
+        GameState{
+            move_count: 0,
+            winner : GameEnd::None,
+            current_player: 0,
+            board_width: w,
+            board_height: h,
+            board: vec![vec![None;h];w],
+        }
+    }
+
+    pub fn draw_board(&self){
+
+        //println!("board size width({});height({})", self.board_width, self.board_height);
+
+        for r in 0..self.board_height{
+            let mut row = "|".to_string();
+            let mut divider = "+".to_string();
+            for c in 0..self.board_width{
+                divider = divider + "---+";
+                row = row + &format!(" {} ", match self.board[c][r]{
+                    None => " ",
+                    Some(ref p) => &p.color,
+                })[..];
+
+                if c == self.board_width-1{
+                    row = row + "|\n";   
+                    divider = divider + "\n";
+                }else{
+                    row = row + "|"
+                }
+            }
+            print!("{divider}");
+            print!("{row}");
+            if r == self.board_height-1{
+                print!("{divider}");
+            }
+        }
+    }
+
+    pub fn put_dot_in(&mut self, c: usize, players : &'a Vec<Player>) -> Result<(), &'static str>{
+        let c = c - 1;
+        if c >= self.board_width { //does not require lower bound check because of usize type
+            return Err("Column index out of bounds");
+        }
+        if self.board[c][0] != None {
+            return Err("Column is full");
+        }
+        for r in 1..self.board_height{
+            if self.board[c][r] != None {
+                self.board[c][r-1] = Some(&players[self.current_player]);
+
+                if self.current_player+1 >= players.len(){
+                    self.current_player = 0;
+                }else{
+                    self.current_player +=1;
+                }
+
+                self.move_count += 1;
+                break;
+            }
+            if r == self.board_height-1{
+                self.board[c][r] = Some(&players[self.current_player]);
+                
+                if self.current_player+1 >= players.len(){
+                    self.current_player = 0;
+                }else{
+                    self.current_player +=1;
+                }
+
+                self.move_count += 1;
+                break;
+            }
+        }
+        Ok(())
+    }
+
+    pub fn set_winner(&mut self){
+        if self.move_count as usize >= self.board_height * self.board_width {
+            self.winner = GameEnd::Draw;
+        }
+
+        for c in 0..self.board_width{
+            for r in 0..self.board_height-3{
+                if self.board[c][r] != None && self.board[c][r] == self.board[c][r+1] &&
+                    self.board[c][r] == self.board[c][r+2] &&
+                    self.board[c][r] == self.board[c][r+3] {
+                    self.winner = GameEnd::Win(self.board[c][r].unwrap());
+                } 
+            }
+        }
+
+        for r in 0..self.board_height{
+            for c in 0..self.board_width-3{
+                if self.board[c][r] != None && self.board[c][r] == self.board[c+1][r] &&
+                    self.board[c][r] == self.board[c+2][r] &&
+                    self.board[c][r] == self.board[c+3][r] {
+                    self.winner = GameEnd::Win(self.board[c][r].unwrap());
+
+                } 
+            }
+        }
+
+        //----------- diagonals -----------------------
+        for row in 0..self.board_height-3{
+            let mut c = 0;
+            for r in row..self.board_height-3{
+                //self.visualize_left(c, r);
+                if self.board[c][r] != None &&
+                    self.board[c][r] == self.board[c+1][r+1] &&
+                    self.board[c][r] == self.board[c+2][r+2] &&
+                    self.board[c][r] == self.board[c+3][r+3] {
+                    self.winner = GameEnd::Win(self.board[c][r].unwrap());
+                }
+                c+=1;
+                if c+3 >= self.board_width{
+                    break;
+                }
+            }
+        }
+
+        for column in 1..self.board_width-3{
+            let mut r = 0;
+            for c in column..self.board_width-3{
+                //self.visualize_left(c,r);
+                if self.board[c][r] != None &&
+                    self.board[c][r] == self.board[c+1][r+1] &&
+                    self.board[c][r] == self.board[c+2][r+2] &&
+                    self.board[c][r] == self.board[c+3][r+3] {
+                    self.winner = GameEnd::Win(self.board[c][r].unwrap());
+                }
+                r+=1;
+            }
+        }
+
+        for row in 0..self.board_height-3{
+            let mut c = self.board_width-1;
+            for r in row..self.board_height-3{
+                //self.visualize_right(c,r);
+                if self.board[c][r] != None &&
+                    self.board[c][r] == self.board[c-1][r+1] &&
+                    self.board[c][r] == self.board[c-2][r+2] &&
+                    self.board[c][r] == self.board[c-3][r+3] {
+                    self.winner = GameEnd::Win(self.board[c][r].unwrap());
+                }
+                c-=1;
+                if c as i8 -3 <= 0{
+                    break;
+                }
+            }
+        }
+
+        for column in (3..self.board_width-1).rev(){
+            let mut r = 0;
+            for c in (3..=column).rev(){
+                //self.visualize_right(c,r);
+                if self.board[c][r] != None &&
+                    self.board[c][r] == self.board[c-1][r+1] &&
+                    self.board[c][r] == self.board[c-2][r+2] &&
+                    self.board[c][r] == self.board[c-3][r+3] {
+                    self.winner = GameEnd::Win(self.board[c][r].unwrap());
+                }
+                r+=1;
+            }
+        }
+    }
+
+    fn  visualize_left(&self, c: usize, r: usize){
+        let mut vis = GameState::new(self.board_width, self.board_height);
+        let empty = Player::empty();
+        vis.board[c][r] = Some(&empty);
+        vis.board[c+1][r+1] = Some(&empty);
+        vis.board[c+2][r+2] = Some(&empty);
+        vis.board[c+3][r+3] = Some(&empty);
+        vis.draw_board();
+    }
+    fn  visualize_right(&self, c: usize, r: usize){
+        let mut vis = GameState::new(self.board_width, self.board_height);
+        let empty = Player::empty();
+        vis.board[c][r] = Some(&empty);
+        vis.board[c-1][r+1] = Some(&empty);
+        vis.board[c-2][r+2] = Some(&empty);
+        vis.board[c-3][r+3] = Some(&empty);
+        vis.draw_board();
+    }
+}
+
+
+pub fn ask_for_input() -> String{
+    let mut s = String::new();
+    stdout().flush().expect("flush failed!");
+    stdin().read_line(&mut s).expect("did not enter a correct string");
+    if let Some('\n')=s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r')=s.chars().next_back() {
+        s.pop();
+    }
+    s
+}
---
+++ src/main.rs
@@ -0,0 +1,69 @@
+use connect_four::{GameState, ask_for_input, GameEnd, Player, COLORS};
+
+fn main() {
+    
+    let mut players = Vec::new();
+
+    print!("Player Count: ");
+    let player_count = match ask_for_input().parse::<u8>(){
+            Err(e) => {println!("parsing error: {e}");return;}
+            Ok(n) if n <= 8 => n,
+            Ok(n) => {println!("player count {n} is too big");return;}
+    };
+
+    for i in 1..=player_count{
+        print!("Player {i}'s name : ");
+        let p_name = ask_for_input();
+        players.push(Player::new(p_name.to_owned(), COLORS[i as usize -1].to_string()));
+    }
+    
+    print!("Width: ");
+    let width = match ask_for_input().parse::<u8>(){
+            Err(e) => {println!("parsing error: {e}");return;}
+            Ok(n) if n >= 4 => n,
+            Ok(n) => {println!("width {n} is too small");return;}
+    };
+
+    print!("Height: ");
+    let height = match ask_for_input().parse::<u8>(){
+            Err(e) => {println!("parsing error: {e}");return;}
+            Ok(n) if n >= 4 => n,
+            Ok(n) => {println!("height {n} is too small");return;}
+    };
+
+    let mut gm = GameState::new(width.into(), height.into());
+
+    loop{
+
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char); //clear screen
+
+        gm.draw_board();
+        print!("{}'s turn : ", players[gm.current_player].name);
+        let column = match ask_for_input().parse::<u8>(){
+            Err(e) => {println!("parsing error: {e}");continue;}
+            Ok(n) => n,
+        };
+        match gm.put_dot_in(column.into(), &players){
+            Err(e) => {println!("{e}");continue;}
+            Ok(_) => (),
+        }
+
+        gm.set_winner();
+        match gm.winner{
+            GameEnd::Win(ref player) => {
+                print!("{esc}[2J{esc}[1;1H", esc = 27 as char); //clear screen
+                gm.draw_board();
+                println!("And the winner is {}", player.name);
+                break;
+            }
+            GameEnd::Draw => {
+                gm.draw_board();
+                println!("Draw");
+                break;
+            }
+            GameEnd::None => continue,
+        }
+
+    }
+}
+
